e9311aac7a4507847a9596cea8667016e2bee150
SuzuNagano-kinto <s.nagano@grapple.co.jp>
2020-12-07 13:04:59 +0900

    [Fix]💄 フレーム部分のコンポーネント作成

--- components/frame.vue
+++ components/frame.vue
@@ -1,24 +1,51 @@
 <template>
   <div class="m-frame">
     <svg :viewBox="viewBox" class="m-frame__svg">
-      <mask id="myMask">
-        <!-- Everything under a white pixel will be visible -->
-        <rect x="0" y="0" :width="width" :height="height" fill="white" />
-        <g class="m-frame__tl">
-          <circle cx="20" cy="20" r="20" fill="black" />
-        </g>
-        <g class="m-frame__tr">
-          <circle cx="20" cy="20" r="20" fill="black" />
-        </g>
-        <g transform="translate(-10 780)" class="m-frame__bl">
-          <circle cx="20" cy="20" r="20" fill="black" />
-        </g>
-        <g transform="translate(350 780)" class="m-frame__br">
-          <circle cx="20" cy="20" r="20" fill="black" />
-        </g>
-      </mask>
-      <!-- with this mask applied, we "punch" a heart shape hole into the circle -->
-      <rect x="0" y="0" class="m-frame__bg" />
+      <g class="m-frame__outer">
+        <mask id="mask1">
+          <!-- Everything under a white pixel will be visible -->
+          <rect x="0" y="0" :width="width" :height="height" fill="white" />
+          <g class="m-frame__tl">
+            <circle cx="0" cy="0" r="20" fill="black" />
+          </g>
+          <g class="m-frame__tr">
+            <circle cx="0" cy="0" r="20" fill="black" />
+          </g>
+          <g class="m-frame__bl">
+            <circle cx="0" cy="0" r="20" fill="black" />
+          </g>
+          <g class="m-frame__br">
+            <circle cx="0" cy="0" r="20" fill="black" />
+          </g>
+        </mask>
+        <rect x="0" y="0" class="m-frame__bd" />
+      </g>
+      <g class="m-frame__inr">
+        <mask id="mask2">
+          <!-- Everything under a white pixel will be visible -->
+          <rect x="0" y="0" :width="width - 8" :height="height - 8" fill="white" />
+          <g class="m-frame__tl">
+            <circle cx="0" cy="0" r="24" fill="black" />
+          </g>
+          <g class="m-frame__tr">
+            <circle cx="0" cy="0" r="24" fill="black" />
+          </g>
+          <g class="m-frame__bl">
+            <circle cx="0" cy="0" r="24" fill="black" />
+          </g>
+          <g class="m-frame__br">
+            <circle cx="0" cy="0" r="24" fill="black" />
+          </g>
+        </mask>
+        <!-- with this mask applied, we "punch" a heart shape hole into the circle -->
+        <rect x="0" y="0" class="m-frame__bg" />
+        <!-- <rect x="0" y="0" class="m-frame__test" /> -->
+        <image
+          v-if="this.$store.state.global.pageName === 'index'"
+          xlink:href="@/assets/img/bg.png"
+          class="m-frame__img"
+        />
+      </g>
     </svg>
   </div>
 </template>
@@ -69,10 +96,6 @@ export default Vue.extend({
   z-index: 1;
   &__svg {
     position: relative;
-    rect {
-      width: 100%;
-      height: 100%;
-    }
     mask {
       position: absolute;
       g {
@@ -80,32 +103,86 @@ export default Vue.extend({
       }
     }
   }
-  &__tl {
-    transform: translate(-10px, -10px);
-  }
-  &__tr {
-    transform: translate(100%, -10px);
-    circle {
-      transform: translateX(-30px);
-    }
-  }
-  &__bl {
-    transform: translate(-10px, 100%);
-    circle {
-      transform: translateY(-30px);
-    }
-  }
-  &__br {
-    transform: translate(100%, 100%);
-    circle {
-      transform: translate(-30px, -30px);
-    }
-  }
-  &__bg {
+  &__bd {
     width: 100%;
     height: 100%;
+    fill: $brown_dark2;
+    mask: url(#mask1);
+  }
+  &__bg {
+    width: calc(100% - 8px);
+    height: calc(100% - 8px);
     fill: $brown_pale2;
-    mask: url(#myMask);
+    mask: url(#mask2);
+    transform: translate(4px, 4px);
+    rect {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &__test {
+    @extend .m-frame__bg;
+    fill: none;
+    background: url('~@/assets/img/bg.png');
+  }
+  &__img {
+    display: block;
+    // width: calc(100% - 8px);
+    // height: calc(100% - 8px);
+    mask: url(#mask2);
+    opacity: 0.7;
+    transform: translate(4px, 4px);
+  }
+
+  #mask1 {
+    .m-frame {
+      &__tl {
+        transform: translate(10px, 10px);
+      }
+      &__tr {
+        transform: translate(100%, 10px);
+        circle {
+          transform: translateX(-10px);
+        }
+      }
+      &__bl {
+        transform: translate(10px, 100%);
+        circle {
+          transform: translateY(-10px);
+        }
+      }
+      &__br {
+        transform: translate(100%, 100%);
+        circle {
+          transform: translate(-10px, -10px);
+        }
+      }
+    }
+  }
+  #mask2 {
+    .m-frame {
+      &__tl {
+        transform: translate(7px, 7px);
+      }
+      &__tr {
+        transform: translate(100%, 7px);
+        circle {
+          transform: translateX(-14px);
+        }
+      }
+      &__bl {
+        transform: translate(7px, 100%);
+        circle {
+          transform: translateY(-14px);
+        }
+      }
+      &__br {
+        transform: translate(100%, 100%);
+        circle {
+          transform: translate(-14px, -14px);
+        }
+      }
+    }
   }
 }
 </style>
