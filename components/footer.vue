5b869acf4afc68180f946fda8848b3d094acbf0d
SuzuNagano-kinto <s.nagano@grapple.co.jp>
2020-12-22 15:36:32 +0900

    fix]🍱Footer作成、JSON移動

---
+++ components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="l-footer">
+    <small class="m-footer__copy">2021 Suzu Nagano</small>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.m-footer {
+  &__copy {
+    display: block;
+    padding: spvw(10px);
+    text-align: center;
+    @include josefin-b();
+    font-size: spfz(12px);
+  }
+}
+</style>
